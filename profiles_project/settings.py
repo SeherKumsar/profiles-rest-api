726dd0faf4c673d0b51ada6120746532723aadc3
SeherKumsar <seherkumsar99@outlook.com>
2024-05-16 22:18:29 +0300

    Changed host

--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'j-86ub-cd03gd4jqte!%w3+k^uy7q)gug16d%t)%6416))&3i*'
 DEBUG = bool(int(os.environ.get('DEBUG', 1)))
 
 ALLOWED_HOSTS = [
-    'ec2-34-241-252-179.eu-west-1.compute.amazonaws.com',
+    'ec2-3-252-139-85.eu-west-1.compute.amazonaws.com',
     '127.0.0.1'
 ]
 
