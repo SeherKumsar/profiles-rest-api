a0a356d5542c68e0b79e226511d613a6c713b210
SeherKumsar <seherkumsar99@outlook.com>
2024-05-16 23:01:14 +0300

    Fixed setup.sh

--- deploy/setup.sh
+++ deploy/setup.sh
@@ -5,55 +5,38 @@ set -e
 # TODO: Set to URL of git repo.
 PROJECT_GIT_URL='https://github.com/SeherKumsar/profiles-rest-api.git'
 
-# PROJECT_BASE_PATH='/usr/local/apps/profiles-rest-api'
-# PROJECT_BASE_PATH='/usr/local/apps/django-rest-proj1'
-PROJECT_BASE_PATH='/usr/local/apps'
-VIRTUALENV_BASE_PATH='/usr/local/virtualenvs'
-
-# Set Ubuntu Language
-locale-gen en_GB.UTF-8
+PROJECT_BASE_PATH='/usr/local/apps/profiles-rest-api'
 
 echo "Installing dependencies..."
 apt-get update
 apt-get install -y python3-dev python3-venv python3-pip supervisor nginx git
-# sqlite python-pip
+# sqlite3
 
 # Create project directory
 mkdir -p $PROJECT_BASE_PATH
-# git clone $PROJECT_GIT_URL $PROJECT_BASE_PATH
-git clone $PROJECT_GIT_URL $PROJECT_BASE_PATH/django-rest-proj1
+git clone $PROJECT_GIT_URL $PROJECT_BASE_PATH
 
 # Create virtual environment
-# mkdir -p $PROJECT_BASE_PATH/env
-mkdir -p $VIRTUALENV_BASE_PATH
-# python3 -m venv $PROJECT_BASE_PATH/env
-python3 -m venv $VIRTUALENV_BASE_PATH/profiles_api
+mkdir -p $PROJECT_BASE_PATH/env
+python3 -m venv $PROJECT_BASE_PATH/env
 
 # Install python packages
-# $PROJECT_BASE_PATH/env/bin/pip install -r $PROJECT_BASE_PATH/requirements.txt
-# $VIRTUALENV_BASE_PATH/profiles_api/bin/pip install -r $PROJECT_BASE_PATH/profiles-rest-api/requirements.txt
-$VIRTUALENV_BASE_PATH/profiles_api/bin/pip install -r $PROJECT_BASE_PATH/django-rest-proj1/requirements.txt
+$PROJECT_BASE_PATH/env/bin/pip install -r $PROJECT_BASE_PATH/requirements.txt
 # $PROJECT_BASE_PATH/env/bin/pip install uwsgi==2.0.18
 
-# # Run migrations and collectstatic
-# cd $PROJECT_BASE_PATH
-# $PROJECT_BASE_PATH/env/bin/python manage.py migrate
-# $PROJECT_BASE_PATH/env/bin/python manage.py collectstatic --noinput
-
-# Run migrations
-# cd $PROJECT_BASE_PATH/profiles-rest-api/src
-cd $PROJECT_BASE_PATH/django-rest-proj1/src
+# Run migrations and collectstatic
+cd $PROJECT_BASE_PATH
+$PROJECT_BASE_PATH/env/bin/python manage.py migrate
+$PROJECT_BASE_PATH/env/bin/python manage.py collectstatic --noinput
 
 # Configure supervisor
-# cp $PROJECT_BASE_PATH/deploy/supervisor_profiles_api.conf /etc/supervisor/conf.d/profiles_api.conf
-cp $PROJECT_BASE_PATH/django-rest-proj1/deploy/supervisor_profiles_api.conf /etc/supervisor/conf.d/profiles_api.conf
+cp $PROJECT_BASE_PATH/deploy/supervisor_profiles_api.conf /etc/supervisor/conf.d/profiles_api.conf
 supervisorctl reread
 supervisorctl update
 supervisorctl restart profiles_api
 
 # Configure nginx
-# cp $PROJECT_BASE_PATH/deploy/nginx_profiles_api.conf /etc/nginx/sites-available/profiles_api.conf
-cp $PROJECT_BASE_PATH/django-rest-proj1/deploy/nginx_profiles_api.conf /etc/nginx/sites-available/profiles_api.conf
+cp $PROJECT_BASE_PATH/deploy/nginx_profiles_api.conf /etc/nginx/sites-available/profiles_api.conf
 rm /etc/nginx/sites-enabled/default
 ln -s /etc/nginx/sites-available/profiles_api.conf /etc/nginx/sites-enabled/profiles_api.conf
 systemctl restart nginx.service
